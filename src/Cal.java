ee73a4a402743c077731ef38d7d250abbca3c08b
shocker945516 <shocker945516@gmail.com>
2022-06-16 22:36:55 -0700

    done

---
+++ src/Cal.java
@@ -0,0 +1,44 @@
+public class Cal {
+    private double num1;
+    private double num2;
+
+    public Cal() {
+    }
+
+    public Cal(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void add(Double num1, Double num2) {
+        double result = num1 + num2;
+        System.out.printf("%.0f%s%.0f%s%.0f%n", num1, "+", num2, "=", result);
+    }
+
+    public void subtract(Double num1, Double num2) {
+        double result = num1 - num2;
+        System.out.printf("%.0f%s%.0f%s%.0f%n", num1, "-", num2, "=", result);
+    }
+
+    public void multiply(Double num1, Double num2) {
+        double result = num1 * num2;
+        System.out.printf("%.0f%s%.0f%s%.0f%n", num1, "*", num2, "=", result);
+    }
+
+    public void divide(Double num1, Double num2) {
+        if (num2.equals(0.0)) {
+            System.out.println("Can't divide by zero");
+        } else {
+            double result = num1 / num2;
+            System.out.printf("%.0f%s%.0f%s%.2f%n", num1, "/", num2, "=", result);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        Cal calculator = new Cal();
+        while (true) {
+
+            System.out.println("Please type the first number: ");
+            Double firstNum = input.nextDouble();
+
+            System.out.println("Choose an operation: ");
+            System.out.println(">Type + for addition");
+            System.out.println(">Type - for subtraction");
+            System.out.println(">Type * for multiply");
+            System.out.println(">Type / for divide");
+            String oper = input.next();
+
+            System.out.println("Please type the second number: ");
+            Double secondNum = input.nextDouble();
+            if (oper.equals("+")) {
+                calculator.add(firstNum, secondNum);
+            } else if (oper.equals("-")) {
+                calculator.subtract(firstNum, secondNum);
+            } else if (oper.equals("*")) {
+                calculator.multiply(firstNum, secondNum);
+            } else if (oper.equals("/")) {
+                calculator.divide(firstNum, secondNum);
+            } else {
+                System.out.println("Please enter a valid operator");
+            }
+
+            System.out.println("Do you want to continue or quit? ");
+            String continueOrNot = input.next();
+            if (continueOrNot.toLowerCase().equals("continue")) {
+                continue;
+            } else if (continueOrNot.toLowerCase().equals("quit")) {
+                System.out.println("Bye!");
+                break;
+            } else {
+                System.out.println("I'd take it as 'quit' anyway. Bye!");
+                break;
+            }
+        }
+        input.close();
+    }
+}
